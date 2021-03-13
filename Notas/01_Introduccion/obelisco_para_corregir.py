7ad00f6739480620a5cf918e865dcfef2dab249f
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-13 17:09:01 -0300

    agregado archivo obelisco.py corregido

--- Notas/01_Introduccion/obelisco_para_corregir.py
+++ Notas/01_Introduccion/obelisco_para_corregir.py
@@ -1,5 +1,6 @@
 # obelisco.py
-grosor_billete = 0.11 * 0.001  # grosor de un billete en metros
+
+grosor_billete = 0.11 * 0.001 # 0.11 mm en metros
 altura_obelisco = 67.5         # altura en metros
 num_billetes = 1
 dia = 1
