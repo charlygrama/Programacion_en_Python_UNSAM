544daf1a65bfd5faf15fe7b673307d74eac8c1a4
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-13 22:55:40 -0300

    se agrega modificacion a adelantos

--- Notas/ejercicios_python/Clase01/adelantos.py
+++ Notas/ejercicios_python/Clase01/adelantos.py
@@ -5,9 +5,19 @@ saldo = 500000.0
 tasa = 0.05
 pago_mensual = 2684.11
 total_pagado = 0.0
+total_meses = 0
+adelanto = 0
 
 while saldo > 0:
-    saldo = saldo * (1+tasa/12) - pago_mensual
-    total_pagado = total_pagado + pago_mensual
+    if adelanto < 12000:
+        saldo = saldo * (1 + tasa/12) - pago_mensual - 1000 
+        total_pagado = total_pagado + pago_mensual + 1000 
+        total_meses += 1
+        adelanto += 1000
+    else:
+        saldo = saldo * (1 + tasa/12) - pago_mensual  
+        total_pagado = total_pagado + pago_mensual  
+        total_meses += 1
 
-print('Total pagado', round(total_pagado, 2))
+print('Total pagado: ', round(total_pagado, 2))
+print('Total de meses: ', total_meses)
