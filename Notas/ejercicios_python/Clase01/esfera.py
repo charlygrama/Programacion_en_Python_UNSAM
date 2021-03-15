42a63859253e53e878a063ffd80811352e1a1a77
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-14 21:33:50 -0300

    se agrega esfera.py

---
+++ Notas/ejercicios_python/Clase01/esfera.py
@@ -0,0 +1,7 @@
+# Ejercicio 1.13: El volumen de una esfera
+import math
+print('Ingrese el radio de una esfera:')
+radio = int( input(" radio: " ))
+volumen = (4/3) * math.pi * radio**3
+
+print('el volumen de la esfera es: ', volumen)
