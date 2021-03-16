5e8603aed9f877e3c7b90a4f50b2aad04c7ee4f1
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-16 10:52:18 -0300

    se actualiza inclusive.py

--- Notas/ejercicios_python/Clase01/inclusive.py
+++ Notas/ejercicios_python/Clase01/inclusive.py
@@ -3,7 +3,7 @@
 frase = 'todos somos programadores' 
 palabras = frase.split()
 for palabra in palabras:
-        if 'e' in palabra.:
+        if 'e' in palabra:
             
     frase_t = ?
     print(frase_t)
