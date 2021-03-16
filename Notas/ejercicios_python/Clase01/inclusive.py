e37aa72359cb504b4d16ae744f4a9d9b0e79f6e2
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-16 01:42:56 -0300

    se actualiza inclusive.py

--- Notas/ejercicios_python/Clase01/inclusive.py
+++ Notas/ejercicios_python/Clase01/inclusive.py
@@ -1,2 +1,9 @@
 # Ejercicio 1.29: Traductor (rústico) al lenguaje inclusivo
 
+frase = 'todos somos programadores' 
+palabras = frase.split()
+for palabra in palabras:
+        if 'e' in palabra.:
+            
+    frase_t = ?
+    print(frase_t)
