7645dcab4f03f554da3bd9aeb3afad8f3a248f74
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-21 08:45:39 -0300

    se agrega buscar_precio.py  ### Ejercicio 2.7: Buscar precios

--- Notas/ejercicios_python/Clase02/buscar_precio.py
+++ Notas/ejercicios_python/Clase02/buscar_precio.py
@@ -1,15 +1,13 @@
 ### Ejercicio 2.7: Buscar precios
-# costo_total = 0
 def buscar_precio(nombre_fruta):
+    precio = 0
     with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/precios.csv', 'rt') as f:
-        headers = next(f).split()
         for linea in f:
             fila = linea.split(',')
             if fila[0] == nombre_fruta:
                 precio = fila[1]
+                print(precio)    
 
+    return  
                 
-        print(precio)
-                
-
 buscar_precio('Lima')
