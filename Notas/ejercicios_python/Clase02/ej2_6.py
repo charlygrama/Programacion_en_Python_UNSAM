076d0b92d8e2a5a9782073f6a2ccaff971b01504
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-21 19:41:41 -0300

    se actualiza ### Ejercicio 2.6: Transformar un script en una función

--- Notas/ejercicios_python/Clase02/ej2_6.py
+++ Notas/ejercicios_python/Clase02/ej2_6.py
@@ -1,14 +1,17 @@
 ### Ejercicio 2.6: Transformar un script en una función
+'''Computa el precio total del 
+camion (cajones * precio) de un archivo'''
 costo = 0
+import csv
 def costo_camion(costo_camion):
-    
     costo_total = 0
     with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/camion.csv', 'rt') as f:
-        headers = next(f)
-        for linea in f:
-            fila = linea.split(',')
-            precio = int(fila[1]) * float(fila[2])
-            costo_total += precio
+        lineas = csv.reader(f)
+        headers = next(lineas)
+        for linea in lineas:
+            ncajones = int(linea[1])
+            precio = float(linea[2]) 
+            costo_total += ncajones * precio
     
     return costo_total
 
