b1255d6e4a8f2e20bbe6ca14541e611b01cfa3b5
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-21 19:40:24 -0300

    se agrega informe.py ### Ejercicio 2.15: Lista de tuplas

---
+++ Notas/ejercicios_python/Clase02/informe.py
@@ -0,0 +1,20 @@
+### Ejercicio 2.15: Lista de tuplas
+### Ejercicio 2.6: Transformar un script en una función
+'''Computa el precio total del 
+camion (cajones * precio) de un archivo'''
+costo = 0
+import csv
+def costo_camion(costo_camion):
+    costo_total = 0
+    with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/camion.csv', 'rt') as f:
+        lineas = csv.reader(f)
+        headers = next(lineas)
+        for linea in lineas:
+            ncajones = int(linea[1])
+            precio = float(linea[2]) 
+            costo_total += ncajones * precio
+    
+    return costo_total
+
+costo = costo_camion('../Data/camion.csv')
+print('Costo total:', costo)
