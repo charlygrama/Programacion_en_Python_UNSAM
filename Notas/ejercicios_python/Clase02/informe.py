6401d08f9cd601578394f5411c35c94da27f1c4c
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-21 20:03:02 -0300

    se actualiza informe.py ### Ejercicio 2.15: Lista de tuplas terminado

--- Notas/ejercicios_python/Clase02/informe.py
+++ Notas/ejercicios_python/Clase02/informe.py
@@ -1,17 +1,21 @@
 ### Ejercicio 2.15: Lista de tuplas
 costo = 0
+camion = []
 import csv
-def costo_camion(costo_camion):
+from pprint import pprint
+def leer_camion(costo_camion):
     costo_total = 0
     with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/camion.csv', 'rt') as f:
         lineas = csv.reader(f)
         headers = next(lineas)
         for linea in lineas:
+            lote = ((linea[0]), int(linea[1]), float(linea[2]))
             ncajones = int(linea[1])
-            precio = float(linea[2]) 
+            precio = float(linea[2])
+            camion.append(lote)
             costo_total += ncajones * precio
     
-    return costo_total
+    return camion
 
-costo = costo_camion('../Data/camion.csv')
-print('Costo total:', costo)
+camion = leer_camion('../Data/camion.csv')
+pprint(camion)
