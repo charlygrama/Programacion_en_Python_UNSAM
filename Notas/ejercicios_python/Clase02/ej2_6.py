bd509449cf7f1cff5ecb606c52db45c9f6238a59
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-21 07:03:34 -0300

    se agrega ej2_6 ### Ejercicio 2.6: Transformar un script en una funciónn

--- Notas/ejercicios_python/Clase02/ej2_6.py
+++ Notas/ejercicios_python/Clase02/ej2_6.py
@@ -1,8 +1,19 @@
 ### Ejercicio 2.6: Transformar un script en una función
-def costo_camion(nombre_archivo):
-    ...
-    # Tu código
-    ...
+costo = 0
+def costo_camion(costo_camion):
+    
+    with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Clase02/costo_camion.py', 'rt') as g:
+        # headers = next(g)
+        # for linea in g:
+        #     fila = linea.split(',')
+        #     precio = int(fila[1]) * float(fila[2])
+        #     costo_total += precio
+        #     print(costo_total)
+        g.read()
+        costo_total
 
-costo = costo_camion('../Data/camion.csv')
-print('Costo total:', costo)
+costo_camion('../Data/camion.csv')
+# costo = costo_camion('../Data/camion.csv')
+# print('Costo total:', costo)
+
+# costo_camion('Data/camion.csv')
