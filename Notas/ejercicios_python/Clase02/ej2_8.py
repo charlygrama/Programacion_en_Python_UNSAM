627584f2c6332638670244ff121ed94ec07d1104
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-21 10:40:09 -0300

    se actualiza ### Ejercicio 2.8: Administración de errores

---
+++ Notas/ejercicios_python/Clase02/ej2_8.py
@@ -0,0 +1,17 @@
+### Ejercicio 2.6: Transformar un script en una función
+costo = 0
+def costo_camion(costo_camion):
+    costo_total = 0
+    with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/missing.csv', 'rt') as f:
+        try:
+            headers = next(f)
+            for linea in f:
+                fila = linea.split(',')
+                precio = int(fila[1]) * float(fila[2])
+                costo_total += precio
+        except ValueError:
+            print('\nCUIDADO!!!! datos faltantes!!!')
+    return costo_total
+
+costo = costo_camion('../Data/missing.csv')
+print('Costo total:', costo)
