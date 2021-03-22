0ccfbe1511ddb2d9661b5203061ddb29cfbbd45e
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-21 22:17:11 -0300

    se actualiza ### Ejercicio 2.14: Diccionario geringoso.

--- Notas/ejercicios_python/Clase02/diccionario_geringoso.py
+++ Notas/ejercicios_python/Clase02/diccionario_geringoso.py
@@ -2,12 +2,7 @@
 # https://www.delftstack.com/es/howto/python/python-convert-list-into-dictionary/ link para metodo zip
 def diccionario_geringoso(l1):
     l2 = []
-    # t = ('', '', '')
-    d = { 
-        lista[0] : '', 
-        lista[1] : '', 
-        lista[2] : '' 
-    }
+    
     for c in range(0, len(l1)):
         capadepenapa = '' 
         for c in l1[c]:
