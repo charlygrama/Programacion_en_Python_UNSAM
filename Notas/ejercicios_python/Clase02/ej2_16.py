6cbf8981b193aa51205bb9533fd30342324fd783
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-23 05:20:40 -0300

    se agregra v1 de ### Ejercicio 2.17: Diccionarios como contenedores

--- Notas/ejercicios_python/Clase02/ej2_16.py
+++ Notas/ejercicios_python/Clase02/ej2_16.py
@@ -1,22 +1,29 @@
 ### Ejercicio 2.16: Lista de diccionarios
 import csv
-t = []
+lista = []
 camion = {}
+costo = 0.0
 from pprint import pprint
 def leer_camion(costo_camion):
-    # costo = 0.0
+    costo = 0.0
     with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/camion.csv', 'rt') as f:
         lineas = csv.reader(f)
         headers = next(lineas)
         
         for linea in lineas:
             lote = (linea[0]), int(linea[1]), float(linea[2])
-            t.append(lote)
+            lista.append(lote)
             camion = dict(zip(headers, lote)) 
+
+            nombre = linea[0]
+            cajones = int(linea[1])
+            precio = float(linea[2])
+            costo += cajones * precio   
+
             print(camion)
-    return camion
+            # print(costo)
 
-camion = leer_camion('../Data/camion.csv')
+    return camion, costo
 
-
-           
+camion, costo = leer_camion('../Data/camion.csv')
+print('\nse paga al productor: ',costo)
