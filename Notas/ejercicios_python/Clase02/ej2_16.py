b3a375834c3935edf34992e6e34af66dc8f13815
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-23 05:28:57 -0300

    se agregra v1 de ### Ejercicio 2.17: Diccionarios como contenedores

--- Notas/ejercicios_python/Clase02/ej2_16.py
+++ Notas/ejercicios_python/Clase02/ej2_16.py
@@ -9,12 +9,12 @@ def leer_camion(costo_camion):
     with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/camion.csv', 'rt') as f:
         lineas = csv.reader(f)
         headers = next(lineas)
-        
+            
         for linea in lineas:
             lote = (linea[0]), int(linea[1]), float(linea[2])
             lista.append(lote)
             camion = dict(zip(headers, lote)) 
-            
+
             cajones = int(linea[1])
             precio = float(linea[2])
             costo += cajones * precio   
--- Notas/ejercicios_python/Clase02/ej2_17.py
+++ Notas/ejercicios_python/Clase02/ej2_17.py
@@ -1,17 +1,19 @@
 ### Ejercicio 2.17: Diccionarios como contenedores
 import csv
 from pprint import pprint
-precios = {}
+costo = 0.0
+d_precios = {}
 def leer_precios(precios):
-    precios = {}
+    d_precios = {}
     with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/precios.csv', 'rt') as f:
         lineas = csv.reader(f)
         try:
             for linea in lineas:
                 lote = linea[0], float(linea[1])
                 items = [(lote)]
-                precios = dict(items)
-                print(precios)
+                d_precios = dict(items)
+ 
+                print(d_precios)
         except IndexError:
             pass
     return precios                  
