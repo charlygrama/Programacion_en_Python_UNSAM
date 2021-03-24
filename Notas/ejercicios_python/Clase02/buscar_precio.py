5a23d188bd79f4c73fc9283a4821c15a0db4c2ac
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-23 22:38:25 -0300

    se actualiza ### Ejercicio 2.7: Buscar precios terminado

--- Notas/ejercicios_python/Clase02/buscar_precio.py
+++ Notas/ejercicios_python/Clase02/buscar_precio.py
@@ -4,13 +4,12 @@ def buscar_precio(nombre_fruta):
     with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/precios.csv', 'rt') as f:
         for linea in f:
             fila = linea.split(',')
-            if fila[0] == nombre_fruta:
+            if fila[0] == nombre_fruta.capitalize():
                 encontrado = True
                 precio = fila[1]
-                print(precio)
-            
+                print('El precio de la',nombre_fruta,'es de: ', precio)
 
         if encontrado == False:
-            print('la fruta no figura en el listado')          
-                
+            print ('la fruta',nombre_fruta, 'no figura en el listado')          
+
 buscar_precio('Papa')
