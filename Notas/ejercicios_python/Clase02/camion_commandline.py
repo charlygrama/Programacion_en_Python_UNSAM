dac14c875c84726bbb0534c58c8a22949a7e8ff4
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-23 22:56:15 -0300

    se actualiza ### Ejercicio 2.10: Ejecución desde la línea de comandos con parámetros  terminado

--- Notas/ejercicios_python/Clase02/camion_commandline.py
+++ Notas/ejercicios_python/Clase02/camion_commandline.py
@@ -1,22 +1,22 @@
 ### Ejercicio 2.10: Ejecución desde la línea de comandos con parámetros
 import csv
 import sys
-costo = 0
+import csv
 def costo_camion(costo_camion):
-    costo_total = 0
-    with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/camion.csv', 'rt') as f:
-        lineas = csv.reader(f)
-        headers = next(lineas)
+    costo = 0.0
+    with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/camion.csv', 'rt') as c:
+        lineas = csv.reader(c)
+        next(lineas)
         for linea in lineas:
-            precio = int(linea[1]) * float(linea[2])
-            costo_total += precio
+            cajones = int(linea[1])
+            precio = float(linea[2])
+            costo += cajones * precio 
+    return costo
 
-    if len(sys.argv) == 2:
-        camion_commandline = sys.argv[1]
-    else:
-        camion_commandline = '../Data/camion.csv'
-
-    return costo_total
+if len(sys.argv) == 2:
+    camion_commandline = sys.argv[1]
+else:
+    camion_commandline = '../Data/camion.csv'
 
 costo = costo_camion('camion_commandline')
-print('Costo total:', costo)
+print('El costo del camión es: ', costo)
