b759897eb08615f885569bd983b460fb18f1571e
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-24 20:46:54 -0300

    se actualiza solucion_errores.py

--- Notas/ejercicios_python/Clase03/solucion_de_errores.py
+++ Notas/ejercicios_python/Clase03/solucion_de_errores.py
@@ -9,20 +9,46 @@
             # * cuando termina de recorrer la expresion retorna el valor.
 #    A continuación va el código corregido
 
-def tiene_a(expresion):
-    n = len(expresion)
-    i = 0
-    while i<n:
-        if expresion[i] == 'a':
-            return True
-        else:
-            a = False
-        i += 1
-    return a
-tiene_a('UNSAM 2020')
-tiene_a('abracadabra')
-tiene_a('La novela 1984 de George Orwell')
+# def tiene_a(expresion):
+#     n = len(expresion)
+#     i = 0
+#     while i<n:
+#         if expresion[i] == 'a':
+#             return True
+#         else:
+#             a = False
+#         i += 1
+#     return a
+# tiene_a('UNSAM 2020')
+# tiene_a('abracadabra')
+# tiene_a('La novela 1984 de George Orwell')
 
 #%%
-#Ejercicio 3.2. Función tiene_a(), nuevamente
-#Comentario: El error era de TAL tipo y estaba ubicado en TAL lugar.
+#Ejercicio 3.2: Sintaxis
+#Comentario: El error es que las palabras del código estaban mal escritas
+#    Lo corregí :
+            # * definiendo bien las funciones agregando ':'
+            # * agregando '==' en el if
+            # * cambiando el Falso por False
+#    A continuación va el código corregido
+
+# def tiene_a(expresion):
+#     n = len(expresion)
+#     i = 0
+#     while i<n:
+#         if expresion[i] == 'a':
+#             return True
+#         i += 1
+#     return False
+
+# tiene_a('UNSAM 2020')
+# tiene_a('La novela 1984 de George Orwell')
+
+#%%
+# Ejercicio 3.3: Tipos
+#Comentario: El error es que las palabras del código estaban mal escritas
+#    Lo corregí :
+            # * definiendo bien las funciones agregando ':'
+            # * agregando '==' en el if
+            # * cambiando el Falso por False
+#    A continuación va el código corregido
