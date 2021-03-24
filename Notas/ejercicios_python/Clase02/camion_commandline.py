80f14a525e3474b6aecd364830484d39ec1afdae
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-24 17:41:05 -0300

    se actualiza camion_commandline.py

--- Notas/ejercicios_python/Clase02/camion_commandline.py
+++ Notas/ejercicios_python/Clase02/camion_commandline.py
@@ -1,10 +1,9 @@
 ### Ejercicio 2.10: Ejecución desde la línea de comandos con parámetros
 import csv
 import sys
-import csv
 def costo_camion(costo_camion):
     costo = 0.0
-    with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/camion.csv', 'rt') as c:
+    with open('../python_UNSAM/Notas/ejercicios_python/Data/camion.csv', 'rt') as c:
         lineas = csv.reader(c)
         next(lineas)
         for linea in lineas:
