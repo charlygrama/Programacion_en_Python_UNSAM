939251b0045b85e1b70647dabfd8a2b25c643f0d
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-24 23:06:37 -0300

    se agrega Ejercicio 3.8: Un ejemplo práctico de enumerate()

---
+++ Notas/ejercicios_python/Clase03/costo_camion.py
@@ -0,0 +1,15 @@
+### Ejercicio 2.2: Lectura de un archivo de datos
+import csv
+def costo_camion(costo_camion):
+    costo = 0.0
+    with open('g:/proyectos/python/python_UNSAM/Notas/ejercicios_python/Data/camion.csv', 'rt') as c:
+        lineas = csv.reader(c)
+        next(lineas)
+        for linea in lineas:
+            cajones = int(linea[1])
+            precio = float(linea[2])
+            costo += cajones * precio 
+    return costo
+
+costo = costo_camion('../Data/missing.csv')
+print('El costo del camión es: ', costo)
