c6090bdcaddcb157525c6367e543cd19b4321d0b
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-24 22:03:08 -0300

    se actualiza solucion_errores.py terminado

--- Notas/ejercicios_python/Clase03/solucion_de_errores.py
+++ Notas/ejercicios_python/Clase03/solucion_de_errores.py
@@ -9,19 +9,19 @@
             # * cuando termina de recorrer la expresion retorna el valor.
 #    A continuación va el código corregido
 
-# def tiene_a(expresion):
-#     n = len(expresion)
-#     i = 0
-#     while i<n:
-#         if expresion[i] == 'a':
-#             return True
-#         else:
-#             a = False
-#         i += 1
-#     return a
-# tiene_a('UNSAM 2020')
-# tiene_a('abracadabra')
-# tiene_a('La novela 1984 de George Orwell')
+def tiene_a(expresion):
+    n = len(expresion)
+    i = 0
+    while i<n:
+        if expresion[i] == 'a':
+            return True
+        else:
+            a = False
+        i += 1
+    return a
+tiene_a('UNSAM 2020')
+tiene_a('abracadabra')
+tiene_a('La novela 1984 de George Orwell')
 
 #%%
 #Ejercicio 3.2: Sintaxis
@@ -32,17 +32,17 @@
             # * cambiando el Falso por False
 #    A continuación va el código corregido
 
-# def tiene_a(expresion):
-#     n = len(expresion)
-#     i = 0
-#     while i<n:
-#         if expresion[i] == 'a':
-#             return True
-#         i += 1
-#     return False
+def tiene_a(expresion):
+    n = len(expresion)
+    i = 0
+    while i<n:
+        if expresion[i] == 'a':
+            return True
+        i += 1
+    return False
 
-# tiene_a('UNSAM 2020')
-# tiene_a('La novela 1984 de George Orwell')
+tiene_a('UNSAM 2020')
+tiene_a('La novela 1984 de George Orwell')
 
 #%%
 # Ejercicio 3.3: Tipos
@@ -54,19 +54,19 @@
             # * cambiando el Falso por False
 #    A continuación va el código corregido
 
-# def tiene_uno(expresion):
-#     n = len(expresion)
-#     i = 0
-#     tiene = False
-#     while (i<n) and not tiene:
-#         if expresion[i] == '1':
-#             return True
-#         i += 1
-#     return tiene
+def tiene_uno(expresion):
+    n = len(expresion)
+    i = 0
+    tiene = False
+    while (i<n) and not tiene:
+        if expresion[i] == '1':
+            return True
+        i += 1
+    return tiene
 
-# tiene_uno('UNSAM 2020')
-# tiene_uno('La novela 1984 de George Orwell')
-# tiene_uno('1984')
+tiene_uno('UNSAM 2020')
+tiene_uno('La novela 1984 de George Orwell')
+tiene_uno('1984')
 
 #%%
 # Ejercicio 3.4: Alcances
@@ -77,20 +77,19 @@
 #    A continuación va el código corregido
 
 
-# def suma(a,b):
-#     return a + b 
+def suma(a,b):
+    return a + b 
   
-# a = 2
-# b = 3
-# c = suma(a,b)
-# print(f"La suma da {a} + {b} = {c}")
+a = 2
+b = 3
+c = suma(a,b)
+print(f"La suma da {a} + {b} = {c}")
 
 #%%
 # Ejercicio 3.5: Pisando memoria
-#Comentario: El error es que suma(a,b) devolvia 'none' ya que c no estaba definida y 'a' y 'b' estaban definidas globalmente y 
-# en la funcion sobreescribiendo sus valores 
+#Comentario: El error era que se hacia referencia a una ubicacion incorrecta de camion.csv 
 #    Lo corregí :
-            # * agregando un return a la funcion y eliminando c dentro de la funcion
+            # * modificando la refereencia a ubicacion del archivo
 #    A continuación va el código corregido
 
 import csv
@@ -99,7 +98,7 @@ from pprint import pprint
 def leer_camion(nombre_archivo):
     camion=[]
     registro={}
-    with open('../Data/camion.csv',"rt") as f:
+    with open('../python_UNSAM/Notas/ejercicios_python/Data/camion.csv',"rt") as f:
         filas = csv.reader(f)
         encabezado = next(filas)
         for fila in filas:
