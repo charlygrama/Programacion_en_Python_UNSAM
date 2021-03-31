6b506d9fd3c949e88233825c24ab880ddc8f5f72
carlosgramajo <carlosgramajo76@gmail.com>
2021-03-30 21:30:19 -0300

    ## Ejercicio 3.17: Tablas de multiplicar

--- Notas/ejercicios_python/Clase03/tablamult.py
+++ Notas/ejercicios_python/Clase03/tablamult.py
@@ -1,17 +1,10 @@
 ## Ejercicio 3.17: Tablas de multiplicar
 l = []
 t = 0
-for i in range(0,10):
-    for j in range(0,10):
-        print('{:3}'.format(i * j), end=' ')
-        l.append(i)
+print('-'*40)
+for i in range(10):
+    for j in range(10):
+        print('{:3}'.format(i * j), end = ' ')
+    print()
+print('-'*40)
 
-    # t += 1
-# print(f'{l}')
-
-# for i in l:
-#     print(l[i]) 
-#     i += 1
-# print(f'{nombre:>10s} {cajones:>10d} {precio:>10.2f}')
-# :>10d   Entero alineado a la derecha en un campo de 10 caracteres
-    
